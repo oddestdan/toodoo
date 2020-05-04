4b467243f8e3f26476ed7c3c70792ea23101ce08
Dan <oddestdan@gmail.com>
2020-05-04 18:05:01 +0300

    Fix issues highlighted by mentor/reviewer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,11 @@ import { AppComponent } from './app.component';
 
 import { AddTodoComponent } from './components/add-todo/add-todo.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
-import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { TodoItemComponent } from './components/todo-list/todo-item/todo-item.component';
 import { ValidatorMessageComponent } from './components/validator-message/validator-message.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { NavListComponent } from './components/nav-list/nav-list.component';
+import { NavListItemComponent } from './components/nav-list/nav-list-item/nav-list-item.component';
 
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -23,8 +25,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-import { NavListItemComponent } from './components/nav-list-item/nav-list-item.component';
-import { NavListComponent } from './components/nav-list/nav-list.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -53,6 +54,7 @@ import { NavListComponent } from './components/nav-list/nav-list.component';
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
